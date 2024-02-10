6919a7f8976d893dfdefda860ff810fca82878e7
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-10 18:16:06 +0900

    Update ed.js

--- ed.js
+++ ed.js
@@ -3,20 +3,22 @@ import {deriveKey} from "./derive-key.js";
 export const enc=async(file,pass)=>{
   const iv=window.crypto.getRandomValues(new Uint8Array(12));
   const algo={name:"AES-GCM",iv};
+  const salt=self.crypto.getRandomValues(new Uint8Array(16));
   const key=await deriveKey(pass);
   const data=await (new Blob([iv,file])).arrayBuffer();
   console.log(`enc(${JSON.stringify(algo)},key(${pass}),${new Uint8Array(data)})`);
   const ab=await window.crypto.subtle.encrypt(algo,key,data);
   console.log(`result:${new Uint8Array(ab)}`);
-  return new File([iv,ab],file.name+"-e");
+  return new File([iv,salt,ab],file.name+"-e");
 };
 
 export const dec=async(file,pass)=>{
   const res=await file.arrayBuffer();
-  const ab=res.slice(12,res.byteLength);
   const iv=new Uint8Array(res,0,12);
+  const salt=new Uint8Array(res,12,16);
+  const ab=res.slice(12+16,res.byteLength);
   const algo={name:"AES-GCM",iv};
-  const key=await deriveKey(pass);
+  const key=await deriveKey(pass,salt);
   console.log(`dec(${JSON.stringify(algo)},key(${pass}),${new Uint8Array(ab)})`);
   const rtn=await crypto.subtle.decrypt(algo,key,ab);
   console.log(`result:${new Uint8Array(rtn)}`);
