39fa4943e9090914a7d4679889180963b4f1b737
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-10 15:40:43 +0900

    Create ed.js

---
+++ ed.js
@@ -0,0 +1,23 @@
+import {deriveKey} from "./derive-key.js";
+
+export const enc=async(file,pass)=>{
+  const iv=window.crypto.getRandomValues(new Uint8Array(12));
+  const algo={name:"AES-GCM",iv};
+  const key=await deriveKey(pass);
+  const data=await (new Blob([iv,file])).arrayBuffer();
+  console.log(`enc(${JSON.stringify(algo)},key(${pass}),${new Uint8Array(data)})`);
+  const ab=await window.crypto.subtle.encrypt(algo,key,data);
+  console.log(`result:${new Uint8Array(ab)}`);
+  return ab;
+};
+
+export const dec=async(res,pass)=>{
+  const iv=new Uint8Array(res,0,12);
+  const algo={name:"AES-GCM",iv};
+  const key=await deriveKey(pass);
+  const ab=res.slice(12,res.byteLength);
+  console.log(`dec(${JSON.stringify(algo)},key(${pass}),${new Uint8Array(ab)})`);
+  const rtn=await crypto.subtle.decrypt(algo,key,ab);
+  console.log(`result:${new Uint8Array(rtn)}`);
+  return rtn;
+};
